415b502090a6a514de169dc0686be3f7f4128448
rektide de la faye <rektide@voodoowarez.com>
2024-02-29 02:27:11 -0500

    example-journal-watchdog service, and random.js service, which alas dont work
    seems like it should and mostly behaves ok, but a) service never goes to starting hiss boo and b) the underlying ExecStartPost= eventually times out and everything starts. what a fucking sham systemd, another case where you seem to have all the right pieces but you can't do the damned thing and none of this really works well enough. hiss boo. lame.

---
+++ random.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import {setTimeout} from "node:timers/promises"
+import {isMain} from "is-main"
+
+function dice(x, a, integerize = false) {
+	let sum = 0
+	while (a) {
+		sum += Math.floor(Math.random() * x) + 1
+		--a
+	}
+	return sum
+}
+
+async function delay(x = 6, a = 1, c = 1000) {
+	while (true) {
+		const ms = dice(x, a) * c
+		console.log({ms})
+		await setTimeout(ms)
+	}
+}
+
+async function main(x = process.argv[2], a = process.argv[3]) {
+	return delay(x, a)
+}
+
+if (isMain(import.meta)) {
+	main()
+}
